e0283711043c2950431d6683a37702913a32e694
Shivkumar Hitesh Thakkar <5thaks07@solent.ac.uk>
2021-10-07 11:40:57 +0100

    if else

---
+++ basics/basis.py
@@ -0,0 +1,6 @@
+print("what type of book is this ")
+book = input()
+if book == "adventure":
+    print("i like adevture books")
+
+print(" finished reading the book")
---
+++ basics/if_.py
@@ -0,0 +1,7 @@
+print("what type of book is this ")
+book = input()
+if book == "adventure":
+    print("i like adevture books")
+    print(" finished reading the book")
+else:
+    print("error")
