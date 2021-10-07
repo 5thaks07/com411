8d3d5968a45330d60ef9e6f1e5e378886240da5e
Shivkumar Hitesh Thakkar <5thaks07@solent.ac.uk>
2021-10-07 12:05:26 +0100

    if elif else

---
+++ basics/if elif else _.py
@@ -0,0 +1,14 @@
+print("in which direction should i move the brush")
+direction = input()
+if direction == "down":
+     print ("move brush in downward direction")
+
+elif direction == "right":
+     print ("move the brush on right side")
+
+elif direction == "left":
+     print ("move the brush on left side")
+
+else:
+     print ("up")
+
